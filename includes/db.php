52e835425b21e99e6503c4b70300587fa0d14d66
Your Name <you@example.com>
2025-12-29 22:24:27 +0700

    modified db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,17 @@
 <?php
-$host = 'localhost';
+$host = 'host.docker.internal'; // penting untuk Docker
 $dbname = 'lauk_restoran';
 $username = 'root';
 $password = 'basketismylife';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
-?>
